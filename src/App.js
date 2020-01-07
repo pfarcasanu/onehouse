a19da65f3628a3df73ac141bdbd13d6d9076efb5
tommypraeger <thomaspraeger2020@u.northwestern.edu>
2020-01-07 17:19:08 -0600

    initial firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import firebaseConfig from './Config.js'
 import ListPage from './ListPage'
 
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
+
+const testItems = [
+  { name: 'bread', qty: 1 },
+  { name: 'eggs', qty: 2 }
+];
+
+const saveItem = item => {
+  db.child('items').child(item.id).set(Object.assign(item, { active: true }))
+    .catch(error => alert(error));
+};
+
+const deleteItem = item => {
+  db.child('items').child(item.id).update({active: false})
+    .catch(error => alert(error));
+};
+
+const createItemList = groceryList => ([
+  Object.values(groceryList.items).filter(item => item.active)
+]);
+
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const handleData = snap => {
+      if (snap.val()) setItems(createItemList(snap.val()));
+    };
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
+  }, []);
+
   return (
     <div className="App">
       <ListPage/>
