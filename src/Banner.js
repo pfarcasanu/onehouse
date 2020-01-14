efaabd3f282d4a19a8c7c11eeb9741904f89bf94
Philip Clement <pclement803@gmail.com>
2020-01-13 19:28:54 -0600

    Added support for logging in

--- src/Banner.js
+++ src/Banner.js
@@ -1,12 +1,15 @@
 import React, { Component, useState } from "react";
 import "rbx/index.css";
-import { Container, Notification, Title, Content, Block} from "rbx";
+import { Notification, Title, Content, Block, Message, Button} from "rbx";
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-const Banner = () => {
+const Banner = ({user}) => {
   return (
     <React.Fragment>
       <Block>
-
       </Block>
       <Notification color="warning">
         <Title>OneHouse</Title>
@@ -14,9 +17,34 @@ const Banner = () => {
             The best way to shop for your home!
         </Content>
       </Notification>
+      
+      {user ? <Welcome user={user} /> : <SignIn />}
 
-    </React.Fragment>
+      </React.Fragment>
   );
 };
+
+const uiConfig = {
+  signInFlow: "popup",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false
+  }
+};
+
+const Welcome = ({ user }) => (
+  <Message color="info">
+    <Message.Header>
+      Welcome, {user.displayName}
+      <Button primary onClick={() => firebase.auth().signOut()}>
+        Log out
+      </Button>
+    </Message.Header>
+  </Message>
+);
+
+const SignIn = () => (
+  <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+);
 
 export default Banner;
