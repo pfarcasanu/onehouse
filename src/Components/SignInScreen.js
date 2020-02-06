c8434a185e0b03d25b27a1797d97feef67495b7b
Philip Clement <pclement803@gmail.com>
2020-02-06 14:32:17 -0600

    improved front end fixes

--- src/Components/SignInScreen.js
+++ src/Components/SignInScreen.js
@@ -24,48 +24,89 @@ const SignInScreen = ({ user }) => {
 
   return (
     <Container>
+      <Notification color="white"></Notification>
       <Title size={2}>Welcome {first}!</Title>
       <Block />
-      <Title subtitle size={4}>
+      <Title subtitle size={3}>
         To get started:
       </Title>
+      <Notification color="white"></Notification>
       <Column.Group>
         <Column size={4} offset={1}>
           <Notification color="info">
-              <Notification color="light">
+            <Notification color="light">
               <Title size={3}>Create a House</Title>
-              </Notification>
-              <Content textAlign="left">
-                  <ul>
-                      <li><Title size={4} substitle> Create a House</Title></li>
-                      <Block/>
-                      <li><Title size={4} substitle> Select a House Key and Password</Title></li>
-                      <Block/>
-                      <li><Title size={4} substitle> Share your Housekey with your Roomates</Title></li>
-                      <Block/>
-                      <li><Title size={4} substitle> Get Shopping!</Title></li>
-                  </ul>
-              </Content>
+            </Notification>
+            <Content textAlign="left">
+              <ul>
+                <li>
+                  <Title size={4} substitle>
+                    {" "}
+                    Create a House
+                  </Title>
+                </li>
+                <Block />
+                <li>
+                  <Title size={4} substitle>
+                    {" "}
+                    Select a House Key and Password
+                  </Title>
+                </li>
+                <Block />
+                <li>
+                  <Title size={4} substitle>
+                    {" "}
+                    Share your Housekey with your Roomates
+                  </Title>
+                </li>
+                <Block />
+                <li>
+                  <Title size={4} substitle>
+                    {" "}
+                    Get Shopping!
+                  </Title>
+                </li>
+              </ul>
+            </Content>
           </Notification>
         </Column>
-        <Column align="centered" size={2} offset={0}>
-        </Column>
+        <Column align="centered" size={2} offset={0}></Column>
         <Column size={4} offset={0}>
           <Notification color="info">
-              <Notification color="light">
+            <Notification color="light">
               <Title size={3}> Join a House</Title>
-              </Notification>
-              <Content textAlign="left">
-                  <ul>
-                      <li><Title size={4} substitle> Join a House</Title></li>
-                      <Block/>
-                      <li><Title size={4} substitle> Get your House Name and Password from your roomates</Title></li>
-                      <Block/>
-                      <li><Title size={4} substitle> Enter your House Name and Password</Title></li>
-                      <Block/>
-                      <li><Title size={4} substitle> Get Shopping!</Title></li>
-                  </ul>
-              </Content>
+            </Notification>
+            <Content textAlign="left">
+              <ul>
+                <li>
+                  <Title size={4} substitle>
+                    {" "}
+                    Join a House
+                  </Title>
+                </li>
+                <Block />
+                <li>
+                  <Title size={4} substitle>
+                    {" "}
+                    Get your House Name and Password from your roomates
+                  </Title>
+                </li>
+                <Block />
+                <li>
+                  <Title size={4} substitle>
+                    {" "}
+                    Enter your House Name and Password
+                  </Title>
+                </li>
+                <Block />
+                <li>
+                  <Title size={4} substitle>
+                    {" "}
+                    Get Shopping!
+                  </Title>
+                </li>
+              </ul>
+            </Content>
           </Notification>
         </Column>
       </Column.Group>
